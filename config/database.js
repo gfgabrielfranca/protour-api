756b79590f0e1df9df6f5e452a42891b69722206
Gabriel França <dev.gabrielfranca@gmail.com>
2019-07-15 21:40:40 -0300

    Finalizando ajustes de banco

--- config/database.js
+++ config/database.js
@@ -1,10 +1,14 @@
 require('./dotenv');
 
+const Url = require('url-parse');
+
+const DATABASE_URL = new Url(process.env.DATABASE_URL);
+
 module.exports = {
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  host: process.env.DB_HOST,
+  username: process.env.DB_USER || DATABASE_URL.username,
+  password: process.env.DB_PASSWORD || DATABASE_URL.password,
+  database: process.env.DB_DATABASE || DATABASE_URL.pathname.substr(1),
+  host: process.env.DB_HOST || DATABASE_URL.hostname,
   dialect: process.env.DB_CONNECTION,
   pool: {
     max: 5,
