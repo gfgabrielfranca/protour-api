4b51e4fcbfd8edad81e1cb386b56a2a61990002b
Gabriel França <dev.gabrielfranca@gmail.com>
2019-08-28 21:17:04 -0300

    Resolvendo bug da migration da reserva

--- database/migrations/20190828235034-change-reservation.js
+++ database/migrations/20190828235034-change-reservation.js
@@ -1,6 +1,7 @@
 module.exports = {
   up: (queryInterface, Sequelize) => queryInterface.addColumn('Reservations', 'totalValue', {
     type: Sequelize.DECIMAL(10, 2),
+    defaultValue: 0,
     allowNull: false,
   }),
   down: queryInterface => queryInterface.removeColumn('Reservations', 'totalValue'),
